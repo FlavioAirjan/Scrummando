c5de1bdef5f827b04d9f2d8cd3cc4b1873d6b52e
Matheus Araujo <matheus.ld.araujo@gmail.com>
2015-05-03 12:44:11 -0300

    Add test.py for Unit Test

--- scrummando/tests.py
+++ scrummando/tests.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 import transaction
 
@@ -13,43 +15,38 @@ class TestMyViewSuccessCondition(unittest.TestCase):
         engine = create_engine('sqlite://')
         from .models import (
             Base,
-            MyModel,
+            Users,
             )
         DBSession.configure(bind=engine)
         Base.metadata.create_all(engine)
         with transaction.manager:
-            model = MyModel(name='one', value=55)
+            model = Users(username='joao',password="senha")
             DBSession.add(model)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
-    def test_passing_view(self):
-        from .views import my_view
+    def test_login_users(self):
+        from scrummando.views import login_user
+        
+        #Creating dummy request
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'scrummando')
+        request.POST["username"] = "joao"
+        request.POST["password"] = "senha"
 
+        info = login_user(request)
+        self.assertEqual(info['status'], 'Usuário logado')
 
-class TestMyViewFailureCondition(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-
-    def tearDown(self):
-        DBSession.remove()
-        testing.tearDown()
-
-    def test_failing_view(self):
-        from .views import my_view
+    def test_add_users(self):
+        from scrummando.views import add_user
+        
+        #Creating dummy request
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info.status_int, 500)
+        request.POST["username"] = "joao"
+        request.POST["password"] = "senha"
+
+        info = add_user(request)
+        self.assertEqual(info['status'], 'ok')
+
+
