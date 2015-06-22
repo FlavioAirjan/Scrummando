e106473041951c03a863bf904e5b4b213c02b302
Matheus Araujo <matheus.ld.araujo@gmail.com>
2015-06-22 17:36:31 -0300

    Fix selenium test

--- selenium_test.py
+++ selenium_test.py
@@ -7,16 +7,16 @@ import sys
 
 class TestFunctional(unittest.TestCase):
   def setUp(self):
-	self.driver = webdriver.Chrome()
+    self.driver = webdriver.Chrome()
 
   def tearDown(self):
     self.driver.close()
 
   def test_website_on(self):
-  	driver = self.driver
-  	driver.get("http://0.0.0.0:6543")
-	assert "Scrummando" in driver.title
-	print "Website on OK"
+    driver = self.driver
+    driver.get("http://192.241.184.37:1212")
+    assert "Scrummando" in driver.title
+    print "Website on OK"
 
 if __name__ == '__main__':
     unittest.main()
